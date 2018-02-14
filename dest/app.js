283e1defe704636cae0676eb215fd53565874d57
Simo Edwin <storyofedd@yahoo.com>
2018-02-14 15:40:37 +0200

    first commit

---
+++ dest/app.js
@@ -0,0 +1,36 @@
+var burger = document.querySelector('.burger');
+var burgerLinks = document.querySelector('.nav-links');
+var burgerLines = document.querySelector('.burger');
+var navBar = document.querySelector('nav');
+var gallery = document.querySelectorAll('.production-gallery img');
+var navLinks = document.querySelectorAll('.nav-links a');
+console.log(gallery);
+
+window.addEventListener('scroll', function () {
+    for (var x = 0; x < gallery.length; x++) {
+        if (gallery[x].getBoundingClientRect().top < 700) {
+            gallery[x].classList.add('reveal-img');
+        }
+    }
+
+});
+
+
+
+burger.addEventListener('click', burgerNavigation);
+window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 70) {
+        navBar.classList.add('nav-animation');
+    } else {
+        navBar.classList.remove('nav-animation');
+    }
+
+});
+
+
+function burgerNavigation() {
+    burgerLinks.classList.toggle('burger-animation');
+    burger.getElementsByTagName('div')[0].classList.toggle('line1-animate');
+    burger.getElementsByTagName('div')[1].classList.toggle('line2-animate');
+    burger.getElementsByTagName('div')[2].classList.toggle('line3-animate');
+}
